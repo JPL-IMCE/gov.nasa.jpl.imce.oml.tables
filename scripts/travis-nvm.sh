eb0706097f2eb03dd4ee1d5adc8dd8643bf85fa3
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-09-19 15:53:53 +0000

    nvm install

--- scripts/travis-nvm.sh
+++ scripts/travis-nvm.sh
@@ -2,7 +2,10 @@
 
 set -ev
 
-if [ ! -d ~/.nvm ]; then
+if [ -d ~/.nvm ]; then
+  echo "# nvm is already installed"
+else
+  echo "# Installing nvm..."
   git clone https://github.com/creationix/nvm.git ~/.nvm
   (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`)
   source ~/.nvm/nvm.sh
